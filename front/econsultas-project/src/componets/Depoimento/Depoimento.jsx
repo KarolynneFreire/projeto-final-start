2f09c505bca7f3ddd1987473b843063f842db05a
RogerioLucenaJr <rogeriofj7@gmail.com>
2023-11-26 20:36:42 -0300

    Alterações na responsividade

--- front/econsultas-project/src/componets/Depoimento/Depoimento.jsx
+++ front/econsultas-project/src/componets/Depoimento/Depoimento.jsx
@@ -10,6 +10,11 @@ function index() {
         <div className="container-dep">
 
             <div className="cards">
+
+            <h3>Renan Albuquerque</h3>
+
+                    <img src={renan} alt="Primeiro depoimento do cliente Renan" />
+
                 <p>
                 Estou bem satisfeito com o serviço.
                     O atendimento do profissional foi ótimo,
@@ -17,37 +22,35 @@ function index() {
                     Com certeza, voltarei a utilizar a plataforma.
                 </p>
 
-                    <img src={renan} alt="Primeiro depoimento do cliente Renan" />
-                
-                <h3>Renan Albuquerque</h3>
-
             </div>
 
             <div className="cards">
 
-                <p>“Encontrei na SuaSaude a solução perfeita para
+                    <h3>Taina Costa</h3>
+
+                    <img src={taina} alt="Primeiro depoimento do cliente Taina" />
+
+                    <p>“Encontrei na SuaSaude a solução perfeita para
                     minha família. Os preços são acessíveis e os
                     médicos são ótimos. Recomendo a todos que
                     precisam de uma alternativa aos planos de
                     saúde tradicionais.”</p>
 
-                    <img src={taina} alt="Primeiro depoimento do cliente Taina" />
-                <h3>Taina Costa</h3>
-
             </div>
 
             <div className="cards">
 
-                <p>Bom, meu primeiro contato com a plataforma
+                    <h3>Givaldo Silva</h3>
+
+                    <img src={givaldo} alt="Primeiro depoimento do cliente Givaldo" />
+
+                    <p>Bom, meu primeiro contato com a plataforma
                     foi excelente. Profissionais qualificados, valor
                     em conta e sem precisar me deslocar. Com
                     certeza irei utilizar desse serviço mais vezes.</p>
-
-                    <img src={givaldo} alt="Primeiro depoimento do cliente Givaldo" />
                 
-                <h3>Givaldo Silva</h3>
-
             </div>
+            
         </div>
     </section>
     </div>
