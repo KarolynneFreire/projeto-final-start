e01574bd1379564fe6a146b63238ba4e68895dbb
RogerioLucenaJr <rogeriofj7@gmail.com>
2023-11-30 00:32:22 -0300

    Finalizando responsividade no index

--- front/econsultas-project/src/componets/Beneficios/Beneficios.jsx
+++ front/econsultas-project/src/componets/Beneficios/Beneficios.jsx
@@ -1,53 +1,65 @@
 import "../Beneficios/assets/css/styles.css"
 import relogio from "../Beneficios/assets/img/5.png"
-import seguranca from "../Beneficios/assets/img/6.png"
-import calendario from "../Beneficios/assets/img/7.png"
+import seguranca from "../Beneficios/assets/img/7.png"
+import calendario from "../Beneficios/assets/img/6.png"
 import estetoscopio from "../Beneficios/assets/img/8.png"
-import mulher from "../Beneficios/assets/img/mulherprof.png"
-
 
 function beneficios() {
   return (
     <div> 
-    
-    <div className="beneficios-container">
-    <div className="item-2">Descubra os 4 beneficios de contratar novos profissionais</div>
 
-    <div className="item-3">Oferecemos planos pensados especialmente para você e sua família. Conheça os 4 benefícios de contratar nossos planos de Assinatura Club Pessoal ou Familiar, e desfrute de uma experiência em telemedicina única e completa.</div>
-    
-    <div className="item-4">
-    <img src={relogio} alt="relogio" />
-    </div>
+      <div className="beneficios-container">
 
-    <div className="item-5">Economia e flexibilidade</div>
+        <div className="beneficios-intro">
 
-    <div className="item-6">Serviços desenvolvidos para proporcionar economia e flexibilidade</div>
+          <h3>Benefícios</h3>
+          <h2>Descubra os 4 beneficios de contratar novos profissionais</h2>
+          <p>Oferecemos planos pensados especialmente para você e sua família. Conheça os 4 benefícios de contratar nossos planos de Assinatura Club Pessoal ou Familiar, e desfrute de uma experiência em telemedicina única e completa.</p>
 
-    <div className="item-7">
-    <img src={seguranca} alt="segurança" />
-    </div>
+        </div>
 
-    <div className="item-8">Segurança e privacidade</div>
+        <div className="container-4-beneficios">
 
-    <div className="item-9">Nossos serviços asseguram a proteção dos seus dados e informações médicas. </div>
+          <div className="beneficios-1-2">
 
-    <div className="item-10">
-    <img src={calendario} alt="calendario" />
-    </div>
+            <div className="beneficio-1">
+            <img src={relogio} alt="relogio" />
+            <h4>Economia e flexibilidade</h4>
+            <p>Serviços desenvolvidos para proporcionar economia e flexibilidade</p>
+            </div>
 
-    <div className="item-11">Agendamento fácil  e rápido</div>
+            <div className="beneficio-2">
+              <img src={seguranca} alt="segurança" />
+              <h4>Segurança e privacidade</h4>
+              <p>Nossos serviços asseguram a proteção dos seus dados e informações médicas.</p>
+            </div>
 
-    <div className="item-12">Facilitamos o agendamento de consultas, permitindo que você marque horários com apenas um clique de qualquer lugar.</div>
+          </div>
 
-    <div className="item-13">
-    <img src={estetoscopio} alt="estetoscopio"/>
-    </div>
+          <div className="beneficios-3-4">
 
-    <div className="item-14">Acesso a diversos profissionais</div>
+            <div className="beneficio-3">
 
-    <div className="item-15">Ao contratar nosso serviço, você poderá ter acesso a uma vasa rede de profissionais qualificados.</div>
-    
-</div>
+              <img src={calendario} alt="calendario" />
+              <h4>Agendamento fácil  e rápido</h4>
+              <p>Facilitamos o agendamento de consultas, permitindo que você marque horários com apenas um clique de qualquer lugar.</p>
+
+            </div>
+
+            <div className="beneficio-4">
+
+              <img src={estetoscopio} alt="estetoscopio"/>
+              <h4>Acesso a diversos profissionais</h4>
+              <p>Ao contratar nosso serviço, você poderá ter acesso a uma vasa rede de profissionais qualificados.</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
 </div>
   )
 }
