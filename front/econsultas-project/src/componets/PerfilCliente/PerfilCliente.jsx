1539d71679c0f80d8ccdb3e3eade175413fc49a9
LeticiaVSiqueira <siqueiraleticia1228@gmail.com>
2023-11-30 14:57:06 -0300

    Criação de dois componetes: Paciente e Profissional.

---
+++ front/econsultas-project/src/componets/PerfilCliente/PerfilCliente.jsx
@@ -0,0 +1,35 @@
+import "./assets/css/styles.css"
+
+function PerfilCliente() {
+    return(
+        
+                <div className='pfc'>
+                
+                <div className="info">
+                <h3>Editar Perfil</h3>
+                </div>
+
+                <div className="user_icon">
+                <img id="icon"src="../src/components/PerfilCliente/assets/img/user_icon.png" alt="imagem" /> 
+                <img id="edit" src="./src/components/PerfilCliente/assets/img/pencil_icon.png" alt="imagem" />
+                </div>
+                
+                <div className="detalhes">
+                <label className="frase" htmlFor="">Nome</label>    
+                <input type="text" class="phrase" placeholder='ex:Maria' 
+                />
+                <label className="frase" htmlFor="">Localização</label>
+                <input type="text" class="phrase" placeholder='ex:Bairro,N°'/>
+                <label className="frase" htmlFor="">Data de nascimento</label>
+                <input type="text" class="phrase" placeholder='ex:00/00/0000'/>
+                <label className="frase" htmlFor="">Gênero</label>
+                <input type="text" class="phrase" placeholder='ex:Cis'/>
+                </div>
+                <button>Salvar</button>
+                </div>
+        
+        
+    )
+}
+
+export default PerfilCliente
