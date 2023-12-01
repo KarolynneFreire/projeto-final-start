96e3062b61d64992fe469083791439390673edc7
jonathafabricio <contato.jonathafabricio@gmail.com>
2023-11-30 23:30:05 -0300

    Página para criar serviço

---
+++ front/econsultas-project/src/pages/CriarServico/CriarServico.jsx
@@ -0,0 +1,75 @@
+
+import React, { useState } from 'react';
+import './assets/css/CriarServico.css'
+
+const CriarServico = () => {
+    const [selecionados, setSelecionados] = useState({}); // Armazena os horários selecionados
+
+    const toggleTimeSlot = (dia, hora) => {
+        const chave = `${dia}-${hora}`;
+        setSelecionados(prevSelecionados => ({
+            ...prevSelecionados,
+            [chave]: !prevSelecionados[chave]
+        }));
+    };
+
+    const horarios = [...Array(24).keys()].map(hour => `${hour.toString().padStart(2, '0')}:00`);
+    const diasDaSemana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+
+    const renderTimeSlots = () => {
+        return horarios.map(hora => (
+            <tr key={hora}>
+                <td>{hora}</td>
+                {diasDaSemana.map(dia => {
+                    const chave = `${dia}-${hora}`;
+                    return (
+                        <td 
+                            key={dia} 
+                            className={`celula-disponibilidade ${selecionados[chave] ? 'selecionado' : ''}`} 
+                            onClick={() => toggleTimeSlot(dia, hora)}
+                        />
+                    );
+                })}
+            </tr>
+        ));
+    };
+
+    return (
+        <div className="pagina-criar-servico">
+            <div className="formulario-criar-servico">
+                <h2>Criar novo serviço</h2>
+                <h3>Título do Servico</h3>
+                <input type="text" placeholder="Dê um título atraente ao seu serviço" />
+                <h3>Valor do serviço</h3>
+                <input type="text" placeholder="Valor do seu serviço" />
+                <h3>Descrição</h3>
+                <textarea placeholder="Faça uma breve descrição do seu serviço"></textarea>
+
+                <h3>Disponibilidade</h3>
+                <table className="tabela-disponibilidade">
+                    <thead>
+                        <tr>
+                            <th>Horário</th>
+                            <th>Segunda</th>
+                            <th>Terça</th>
+                            <th>Quarta</th>
+                            <th>Quinta</th>
+                            <th>Sexta</th>
+                            <th>Sábado</th>
+                            <th>Domingo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderTimeSlots()}
+                    </tbody>
+                </table>
+
+                {/* Aqui você adicionaria os controles para seleção de intervalo entre consultas */}
+
+                <button type="submit">Criar serviço</button>
+            </div>
+        </div>
+    );
+};
+
+export default CriarServico;
