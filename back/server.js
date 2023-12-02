dc40bb282ab9632e9a9e525c388d5406fb32cf6b
jonathafabricio <contato.jonathafabricio@gmail.com>
2023-12-02 19:05:31 -0300

    Funcionalidades para criar serviçoBack,banco,front

--- back/server.js
+++ back/server.js
@@ -46,8 +46,56 @@ app.post('/login', (req, res) => {
             }
         }
         return res.json("Credenciais inválidas");
+    })
+})
+
+app.post('/criar-servico', (req, res) => {
+    const { titulo, valor, descricao, proId } = req.body;
+    const sql = "INSERT INTO ser_servicos (ser_titulo, ser_valor, ser_descricao, status, pro_id) VALUES (?, ?, ?, 'Ativo', ?)";
+    db.query(sql, [titulo, valor, descricao, proId], (err, result) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).json({ message: "Serviço criado com sucesso", serviceId: result.insertId });
+    })
+})
+
+app.get('/criar-servico', (req, res) => {
+    res.send('Endpoint GET /criar-servico (apenas para teste)');
+});
+
+app.put('/atualizar-servico/:id', (req, res) => {
+    const { status } = req.body;
+    const sql = "UPDATE ser_servicos SET status = ? WHERE ser_id = ?";
+    db.query(sql, [status, req.params.id], (err, result) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).json({ message: "Serviço atualizado com sucesso" });
     });
 });
+
+
+app.get('/meus-servicos/:proId', (req, res) => {
+    const sql = "SELECT * FROM ser_servicos WHERE pro_id = ?"
+    db.query(sql, [req.params.proId], (err, results) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).json(results);
+    });
+});
+
+app.delete('/excluir-servico/:id', (req, res) => {
+    const sql = "DELETE FROM ser_servicos WHERE ser_id = ?";
+    db.query(sql, [req.params.id], (err, result) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).json({ message: "Serviço excluído com sucesso" });
+    });
+});
+
 
 app.listen(8081, () => {
     console.log("Servidor rodando na porta 8081");
--- front/econsultas-project/src/pages/CriarServicoProfissional/CriarServicoProfissional.jsx
+++ front/econsultas-project/src/pages/CriarServicoProfissional/CriarServicoProfissional.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 import './assets/css/CriarServicoProfissional.css'
-import Painel from '../../componets/Painel/Painel';
+import Painel from '../../componets/Painel/Painel'
 
 const CriarServicoProfissional = () => {
-    const [selecionados, setSelecionados] = useState({});
+    const [selecionados, setSelecionados] = useState({})
     const navigate = useNavigate()
+    const [titulo, setTitulo] = useState('')
+    const [valor, setValor] = useState('')
+    const [descricao, setDescricao] = useState('')
 
     const toggleTimeSlot = (dia, hora) => {
-        const chave = `${dia}-${hora}`;
+        const chave = `${dia}-${hora}`
         setSelecionados(prevSelecionados => ({
             ...prevSelecionados,
             [chave]: !prevSelecionados[chave]
-        }));
-    };
+        }))
+    }
 
     const handleSubmit = (event) => {
-        event.preventDefault();
-        navigate('/meus-servicos');
-    };
+        event.preventDefault()
+        axios.post('http://localhost:8081/criar-servico', { titulo, valor, descricao, status: 'Ativo', proId: 1 })
+            .then(response => {
+                console.log(response.data)
+                navigate('/meus-servicos')
+            })
+            .catch(error => {
+                console.error('Houve um erro!', error)
+            });
+    }
 
     const horarios = [...Array(24).keys()].map(hour => `${hour.toString().padStart(2, '0')}:00`);
     const diasDaSemana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
@@ -46,13 +57,9 @@ const CriarServicoProfissional = () => {
             <Painel></Painel>
             <div className="pagina-criar-servico">
                 <form className="formulario-criar-servico" onSubmit={handleSubmit}>
-                    <h2>Criar novo serviço</h2>
-                    <h3>Título do Servico</h3>
-                    <input type="text" placeholder="Dê um título atraente ao seu serviço" />
-                    <h3>Valor do serviço</h3>
-                    <input type="text" placeholder="Valor do seu serviço" />
-                    <h3>Descrição</h3>
-                    <textarea placeholder="Faça uma breve descrição do seu serviço"></textarea>
+                    <input type="text" placeholder="Dê um título atraente ao seu serviço" value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+                    <input type="text" placeholder="Valor do seu serviço" value={valor} onChange={(e) => setValor(e.target.value)} />
+                    <textarea placeholder="Faça uma breve descrição do seu serviço" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
 
                     <h3>Disponibilidade</h3>
                     <table className="tabela-disponibilidade">
