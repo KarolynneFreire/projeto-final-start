14fb62f0d590850176e64ba7e857b308e0fadf3f
jonathafabricio <contato.jonathafabricio@gmail.com>
2023-12-02 00:05:57 -0300

    favicon e logo atualizados,

--- front/econsultas-project/src/componets/NavBar/NavBar.jsx
+++ front/econsultas-project/src/componets/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from '../../../public/assets/img/logo.png'
+import logo from '../../assets/img/logo.png'
 
 export default function Navbar() {
   return (
